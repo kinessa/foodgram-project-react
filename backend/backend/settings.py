2624ca9befbcdab6f85c2cd50bf3c3e4d4d0d0dc
kinessa <0309kinessa@gmail.com>
2022-01-17 10:09:36 +0300

    third

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,6 +61,7 @@ DATABASES = {
             'USER': os.environ.get('POSTGRES_USER'),
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
             'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
     }
 }
 
